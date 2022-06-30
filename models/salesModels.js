258f4e1b78252343de11b145a4d8285a8c51b132
vinidipaula <vinidipaula92@gmail.com>
2022-06-30 19:35:43 -0300

    :lipstick: style: Erros de lint alterados

--- models/salesModels.js
+++ models/salesModels.js
@@ -1,25 +1,25 @@
-const connection = require('./connection');
+// const connection = require('./connection');
 
-const salesModel = {
-  async getSales() {
-    const sql = `SELECT saleP.sale_id as saleId, saleP.product_id as productId,
-    saleP.quantity, s.date FROM StoreManager.sales_products as saleP
-    INNER JOIN StoreManager.sales as s 
-    ON saleP.sale_id = s.id
-    ORDER BY saleP.sale_id, saleP.product_id`;;
-    const [rows] = await connection.execute(sql);
-    return rows;
-  },
-  async findById(id) {
-    const sql =
-      `SELECT saleP.product_id as productId,
-    saleP.quantity, s.date FROM StoreManager.sales_products as saleP
-    INNER JOIN StoreManager.sales as s 
-    ON saleP.sale_id = s.id and saleP.sale_id = ?
-    ORDER BY saleP.sale_id, saleP.product_id`;
-    const [[rows]] = await connection.execute(sql, [id]);
-    return rows;
-  },
-}
+// const salesModel = {
+//   async getSales() {
+//     const sql = `SELECT saleP.sale_id as saleId, saleP.product_id as productId,
+//     saleP.quantity, s.date FROM StoreManager.sales_products as saleP
+//     INNER JOIN StoreManager.sales as s 
+//     ON saleP.sale_id = s.id
+//     ORDER BY saleP.sale_id, saleP.product_id`;;
+//     const [rows] = await connection.execute(sql);
+//     return rows;
+//   },
+//   async findById(id) {
+//     const sql =
+//       `SELECT saleP.product_id as productId,
+//     saleP.quantity, s.date FROM StoreManager.sales_products as saleP
+//     INNER JOIN StoreManager.sales as s 
+//     ON saleP.sale_id = s.id and saleP.sale_id = ?
+//     ORDER BY saleP.sale_id, saleP.product_id`;
+//     const [[rows]] = await connection.execute(sql, [id]);
+//     return rows;
+//   },
+// }
 
-module.exports = salesModel;
+// module.exports = salesModel;
